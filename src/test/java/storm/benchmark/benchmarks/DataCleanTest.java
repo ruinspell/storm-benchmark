ede49c5ac87368479019f519c14c5bde4432dd99
manuzhang <owenzhang1990@gmail.com>
2014-08-21 16:46:55 +0800

    fix test

--- src/test/java/storm/benchmark/benchmarks/DataCleanTest.java
+++ src/test/java/storm/benchmark/benchmarks/DataCleanTest.java
@@ -34,13 +34,11 @@ public class DataCleanTest {
     StormBenchmark benchmark = new DataClean();
     Config config = new Config();
     config.put(DataClean.SPOUT_NUM, 3);
-    config.put(DataClean.VIEW_NUM, 4);
     config.put(DataClean.FILTER_NUM, 5);
 
     StormTopology topology = benchmark.getTopology(config);
     assertThat(topology).isNotNull();
     TestUtils.verifyParallelism(Utils.getComponentCommon(topology, DataClean.SPOUT_ID), 3);
-    TestUtils.verifyParallelism(Utils.getComponentCommon(topology, DataClean.VIEW_ID), 4);
     TestUtils.verifyParallelism(Utils.getComponentCommon(topology, DataClean.FILTER_ID), 5);
   }
 }
